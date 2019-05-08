7f1fe7467586989c63a8ce34efb9e3e91083917e
Joee <zyzhou@stu.xmu.edu.cn>
2019-05-08 11:44:07 +0800

    Fix class Money and Date;

--- text.py
+++ text.py
@@ -2,7 +2,7 @@
 """
 TEXT类
 """
-from chn_text_norm.percentage import Percentage
+from chn_text_norm.money import Money
 
 __author__ = 'Zhiyang Zhou <zyzhou@stu.xmu.edu.cn>'
 __data__ = '2019-05-03'
@@ -13,8 +13,12 @@ from chn_text_norm.cardinal import Cardinal
 from chn_text_norm.digit import Digit
 from chn_text_norm.telephone import TelePhone
 from chn_text_norm.fraction import Fraction
-from chn_text_norm.date import date2chn
-from chn_text_norm.money import money2chn
+from chn_text_norm.date import Date
+from chn_text_norm.percentage import Percentage
+
+CURRENCY_NAMES = '(人民币|美元|日元|英镑|欧元|马克|法郎|加拿大元|澳元|港币|先令|芬兰马克|爱尔兰镑|' \
+                 '里拉|荷兰盾|埃斯库多|比塞塔|印尼盾|林吉特|新西兰元|比索|卢布|新加坡元|韩元|泰铢)'
+CURRENCY_UNITS = '((亿|千万|百万|万|千|百)|(亿|千万|百万|万|千|百|)元|(亿|千万|百万|万|千|百|)块|角|毛|分)'
 
 
 class Text:
@@ -28,9 +32,10 @@ class Text:
 
     def _particular(self):
         text = self.norm_text
-        pattern = re.compile(r"(([a-zA-Z]+)二([a-zA-Z]+))")
+        pattern = re.compile(r"(([a-zA-Z]+)二([a-zA-Z]*))")
         matchers = pattern.findall(text)
         if matchers:
+            # print('particular')
             for matcher in matchers:
                 text = text.replace(matcher[0], matcher[1]+'2'+matcher[2])
         self.norm_text = text
@@ -40,10 +45,20 @@ class Text:
         text = self.raw_text
 
         # 规范化日期
-        text = date2chn(text)
+        pattern = re.compile(r"((\d{1,4}年)(\d{1,2}月(\d{1,2}[日号])?)?)")
+        matchers = pattern.findall(text)
+        if matchers:
+            # print('date')
+            for matcher in matchers:
+                text = text.replace(matcher[0], Date(date=matcher[0]).date2chntext())
 
         # 规范化金钱
-        text = money2chn(text)
+        pattern = re.compile(r'((\d+(\.\d+)?)' + CURRENCY_UNITS + '(\d' + CURRENCY_UNITS + '?)?)')
+        matchers = pattern.findall(text)
+        if matchers:
+            # print('money')
+            for matcher in matchers:
+                text = text.replace(matcher[0], Money(money=matcher[0]).money2chntext())
 
         # 规范化固话/手机号码
         # 手机
@@ -74,12 +89,12 @@ class Text:
                 text = text.replace(matcher, Fraction(fraction=matcher).fraction2chntext())
 
         # 规范化百分数
-        pattern = re.compile(r"(\d+\.?\d*%)")
+        pattern = re.compile(r"(\d+(\.\d+)%)")
         matchers = pattern.findall(text)
         if matchers:
             # print('percentage')
             for matcher in matchers:
-                text = text.replace(matcher, Percentage(percentage=matcher).percentage2chntext())
+                text = text.replace(matcher[0], Percentage(percentage=matcher[0]).percentage2chntext())
 
         # 规范化数字编号
         pattern = re.compile(r"(\d{11,32})")
@@ -90,12 +105,12 @@ class Text:
                 text = text.replace(matcher, Digit(digit=matcher).digit2chntext())
 
         # 规范化纯数
-        pattern = re.compile(r"(\d+\.?\d*)")
+        pattern = re.compile(r"(\d+(\.\d+)?)")
         matchers = pattern.findall(text)
         if matchers:
             # print('cardinal')
             for matcher in matchers:
-                text = text.replace(matcher, Cardinal(cardinal=matcher).cardinal2chntext())
+                text = text.replace(matcher[0], Cardinal(cardinal=matcher[0]).cardinal2chntext())
 
         self.norm_text = text
         self._particular()
@@ -106,12 +121,13 @@ class Text:
 if __name__ == '__main__':
 
     # 测试程序
-    print(Text(raw_text='固话：0595-23865596或0595-23880880').normalize())
+    print(Text(raw_text='固话：0595-23865596或23880880').normalize())
     print(Text(raw_text='手机：+86 19859213959。').normalize())
     print(Text(raw_text='分数：32477/76391。').normalize())
     print(Text(raw_text='百分数：32.04%。').normalize())
     print(Text(raw_text='编号：31520181154418。').normalize())
-    print(Text(raw_text='纯数：2983.07和12345.60。').normalize())
-    print(Text(raw_text='日期：今天是9012年12月15日,天气很好').normalize())
-    print(Text(raw_text='金钱：小花有20000澳元，小明借了50.5元花了30.5剩下20块,非常的高兴,那就给小花12块5吧,那我就剩下17块5，小明还欠34.5，小草欠小花20.1万').normalize())
+    print(Text(raw_text='纯数：2983.07或12345.60。').normalize())
+    print(Text(raw_text='日期：1999年2月20日或09年3月15号。').normalize())
+    print(Text(raw_text='金钱：12块5，34.5元，20.1万').normalize())
     print(Text(raw_text='特殊：O2O或B2C。').normalize())
+
